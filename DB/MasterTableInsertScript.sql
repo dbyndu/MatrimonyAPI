cf3b486ce347aa5242e37f432fc4d132432a8a60
Indraraj Majilya <indraraj.bdn@gmail.com>
2020-03-08 21:54:26 +0530

    Master Table insert Script

---
+++ DB/MasterTableInsertScript.sql
@@ -0,0 +1,26 @@
+USE [Matrimony]
+GO
+
+INSERT INTO [dbo].[MasterTableMetadata]([TableName]) VALUES ('Religion')
+INSERT INTO [dbo].[MasterTableMetadata]([TableName]) VALUES ('Country')
+INSERT INTO [dbo].[MasterTableMetadata]([TableName]) VALUES ('Comunity')
+INSERT INTO [dbo].[MasterTableMetadata]([TableName]) VALUES ('ProfileCreator')
+INSERT INTO [dbo].[MasterTableMetadata]([TableName]) VALUES ('MaritalStatus')
+INSERT INTO [dbo].[MasterTableMetadata]([TableName]) VALUES ('Gender')
+INSERT INTO [dbo].[MasterTableMetadata]([TableName]) VALUES ('HealthInfo')
+INSERT INTO [dbo].[MasterTableMetadata]([TableName]) VALUES ('MotherTongue')
+INSERT INTO [dbo].[MasterTableMetadata]([TableName]) VALUES ('ParentStatus')
+INSERT INTO [dbo].[MasterTableMetadata]([TableName]) VALUES ('FamilyType')
+INSERT INTO [dbo].[MasterTableMetadata]([TableName]) VALUES ('FamilyValue')
+INSERT INTO [dbo].[MasterTableMetadata]([TableName]) VALUES ('FamilyAffluence')
+INSERT INTO [dbo].[MasterTableMetadata]([TableName]) VALUES ('EducationLevel')
+INSERT INTO [dbo].[MasterTableMetadata]([TableName]) VALUES ('EducationField')
+INSERT INTO [dbo].[MasterTableMetadata]([TableName]) VALUES ('WorkingSector')
+INSERT INTO [dbo].[MasterTableMetadata]([TableName]) VALUES ('WorkDesignation')
+INSERT INTO [dbo].[MasterTableMetadata]([TableName]) VALUES ('AnualIncome')
+INSERT INTO [dbo].[MasterTableMetadata]([TableName]) VALUES ('Employer')
+INSERT INTO [dbo].[MasterTableMetadata]([TableName]) VALUES ('Smoking')
+INSERT INTO [dbo].[MasterTableMetadata]([TableName]) VALUES ('Drinking')
+GO
+
+
