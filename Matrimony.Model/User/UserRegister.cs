c0cc3fbce885245cf889d367e9839aec8c1a50a2
Srijit Das <srijit.das@cognizant.com>
2020-05-01 22:01:31 +0530

    Percentage Computation

--- Matrimony.Model/User/UserRegister.cs
+++ Matrimony.Model/User/UserRegister.cs
@@ -16,5 +16,6 @@ namespace Matrimony.Model.User
         [JsonIgnore]
         public DateTime? UpdatedDate { get; set; }
         public string ContactName { get; set; }
+        public int? PercentageComplete { get; set; }
     }
 }
